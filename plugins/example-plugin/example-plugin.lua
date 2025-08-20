b712dd234f11d1124c4f7c8cc5ddbc66e0ff69d8
AC4G <vost_400@gmx.de>
2025-08-20 20:18:23 +0200

    Update example-plugin to new schema

--- plugins/example-plugin/example-plugin.lua
+++ plugins/example-plugin/example-plugin.lua
@@ -1,30 +1,17 @@
-local lfs = require("lfs")
+function create(project_name)
+    plugin_ctx:log("Hellow from create_monorepo")
 
-function get_plugin_info()
-    return {
-        name = "example-plugin",
-        version = {
-            major = 1,
-            minor = 0,
-            patch = 0
-        },
-        monorepo_templates = {
-            default = {
-                projects = "services",
-                packages = "libs"
-            }
-        },
-        supports = {
-            "create_monorepo"
-        }
-    }
+    return 0
 end
 
-function create_monorepo(dst_path, template)
-    local status = 0
-    local data_dir = io.data_dir
+function extend(args)
+    return 0
+end
 
-    print("Hellow from create_monorepo")
+function migrate(from_version)
+    return 0
+end
 
+function help()
     return 0
 end
